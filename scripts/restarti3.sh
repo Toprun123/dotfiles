7ae902adc5647eb43975ee62cead876d49c2ce31
Toprun123 <syed.daanish@sixzix.com>
2024-10-02 21:31:16 +0300

    Remove redundant code.

--- scripts/restarti3.sh
+++ scripts/restarti3.sh
@@ -1,15 +1,4 @@
 #!/usr/bin/env bash
 
-LOCKFILE="/tmp/i3-restart.lock"
-
-# Check if the lock file exists
-if [ ! -f "$LOCKFILE" ]; then
-    # Create the lock file to prevent looping
-    touch "$LOCKFILE"
-
-    # Sleep for 2 seconds and restart i3
-    sleep 3
-    i3-msg restart
-fi
-
-sleep 20 && rm -f $LOCKFILE
+sleep 3
+i3-msg restart
