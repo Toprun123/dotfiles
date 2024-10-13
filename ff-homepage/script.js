d9b6c5b972c70907f3a8ba26fded9d748b4a4d2f
Toprun123 <syed.daanish@sixzix.com>
2024-10-13 16:10:20 +0300

    Update script

--- ff-homepage/script.js
+++
@@ -1,7 +0,0 @@
-fetch('https://programming-quotesapi.vercel.app/api/random')
-    .then(response => response.json())
-    .then(quote => {
-        document.getElementById('quote').innerText = quote.quote;
-        document.getElementById('author').innerText = quote.author;
-        console.log(quote);
-    });
