6a329389af098efb149e325f2476610dcb69dcce
Syed Daanish <syed@sixzix.com>
2024-11-28 21:00:20 +0300

    Remove unneccesarry code

--- polybar/dot-config/polybar/launch.sh
+++ polybar/dot-config/polybar/launch.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 if ! pkill polybar; then
-    pkill polybar
     if type "xrandr"; then
         for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
             MONITOR=$m polybar --reload main &
